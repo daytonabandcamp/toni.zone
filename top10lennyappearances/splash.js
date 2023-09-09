798d1a58b1d7a28921fe8ca5d17eda0b30db5aa9
daytonabandcamp <lighninglarryluciano@gmail.com>
2023-09-09 00:54:21 -0700

    lenny fix

--- top10lennyappearances/splash.js
+++ top10lennyappearances/splash.js
@@ -1,6 +1,6 @@
-let lennymidi = new Audio("top10lennyappearances/lenny.mp3");
+let lennymidi = new Audio("lenny.mp3");
 async function getQuotes(){
-    const res = await fetch("top10lennyappearances/quotes.json");
+    const res = await fetch("quotes.json");
     return res.json();
 }
 getQuotes().then(data => {
@@ -10,8 +10,8 @@ function playMidi() {
     lennymidi.play();
 }
 document.getElementById("lenny").addEventListener("mouseover",function(){
-    document.getElementById("lenny").src="img/lenny/lenny2.png"
+    document.getElementById("lenny").src="../img/lenny/lenny2.png"
 })
 document.getElementById("lenny").addEventListener("mouseout",function(){
-    document.getElementById("lenny").src="img/lenny/leonard.png"
+    document.getElementById("lenny").src="../img/lenny/leonard.png"
 })
