238d65cfe2e13e8943a4835b3617ed8b21ad250b
daytonabandcamp <daytonabandcamp@gmail.com>
2025-03-06 18:51:49 -0700

    thumbnailizer update!

--- img/thumb/thumbnailizer.lua
+++ img/thumb/thumbnailizer.lua
@@ -1,3 +1,5 @@
+-- thumbnailizer 1.1: crop it yourself you lazy bastard
+
 function mysplit(inputstr, sep)
     if sep == nil then
       sep = "%s"
@@ -9,16 +11,14 @@ function mysplit(inputstr, sep)
     return t
 end
 
-print("thumbnailizer by daytona\n\ninput: {filename},{crop-side},{text-corner},{text}")
+print("thumbnailizer by daytona\n\ninput: {filename},{text}")
 commands = io.read()
 
 -- FILENAME = comList[1]
--- CROP-SIDE = comList[2]
--- TEXT-CORNER = comList[3]
--- TEXT = comList[4]
+-- TEXT = comList[2]
 
 comList = mysplit(commands, ',')
 
-os.execute("magick "..comList[1]..".png -resize 512x -gravity "..comList[2].." -crop 512x288+0+0 +repage "..comList[1].."-crop.png")
-os.execute("magick "..comList[1].."-crop.png -gravity "..comList[3].." -stroke black -strokewidth 12 -font Ubuntu-Mono -pointsize 108 -annotate +10+0 "..comList[4].." -stroke none -fill white -annotate +12+2 "..comList[4].." "..comList[1].."-thumb.png")
+os.execute("magick "..comList[1]..".png -resize 512x -gravity Center -crop 512x288+0+0 +repage "..comList[1].."-crop.png")
+os.execute("magick "..comList[1].."-crop.png -gravity SouthWest -stroke black -strokewidth 12 -font Ubuntu-Mono -pointsize 108 -annotate +10+0 "..comList[2].." -stroke none -fill white -annotate +12+2 "..comList[2].." "..comList[1].."-thumb.png")
 os.execute("del "..comList[1].."-crop.png")
